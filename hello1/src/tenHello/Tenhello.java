9d7122fcd9070aecfd5ce860026e4a0439db1b90
chenmengxuan6 <15226523963@163.com>
2018-12-13 10:28:43 +0800

    hello1

---
+++ hello1/src/tenHello/Tenhello.java
@@ -0,0 +1,12 @@
+package tenHello;
+
+import org.testng.annotations.Test;
+
+public class Tenhello {
+	@Test
+	public void tenHello() {
+		for(int i=0;i<10;i++) {
+			System.out.println("hello world");
+		}
+	}
+}
